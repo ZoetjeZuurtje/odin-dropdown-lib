0bbb1aeed5a28b4f77da05e5049231c342529f9a
Bram <bramstrik@live.com>
2024-11-28 13:58:53 +0100

    Finish dropdown

--- src/dropdown.js
+++ src/dropdown.js
@@ -1,32 +1,7 @@
 import './index.css'
 
-function fitFirstChild (element, dimensions = 'both') {
-  const firstChildSize = element.children[0].getBoundingClientRect()
-
-  if (dimensions !== 'width') {
-    element.style.height = `${firstChildSize.height}px`
-  }
-  if (dimensions !== 'height') {
-    element.style.width = `${firstChildSize.width}px`
-  }
-}
-
 function toggleDropdown (event) {
-  const dropdown = event.target.parentElement
-
-  try {
-    if (dropdown.style.height === 'fit-content') {
-      fitFirstChild(dropdown)
-    } else {
-      dropdown.style.height = 'fit-content'
-      dropdown.style.width = 'fit-content'
-    }
-  } catch (err) {
-    console.error(err)
-  }
+  event.target.parentElement.classList.toggle('open')
 }
 
-document.querySelectorAll('.dropdown > button').forEach(dropdownButton => {
-  dropdownButton.addEventListener('click', toggleDropdown)
-  fitFirstChild(dropdownButton.parentElement); // collapse it by default
-})
+document.querySelectorAll('.dropdown > button').forEach(dropdownButton => dropdownButton.addEventListener('click', toggleDropdown))
