efb6a64cd9b391f2046dd366ea824b7f6b271e1c
Bram <bramstrik@live.com>
2024-11-28 12:00:36 +0100

    Add dropdown functionality

--- src/dropdown.js
+++ src/dropdown.js
@@ -1,1 +1,32 @@
 import './index.css'
+
+function fitFirstChild (element, dimensions = 'both') {
+  const firstChildSize = element.children[0].getBoundingClientRect()
+
+  if (dimensions !== 'width') {
+    element.style.height = `${firstChildSize.height}px`
+  }
+  if (dimensions !== 'height') {
+    element.style.width = `${firstChildSize.width}px`
+  }
+}
+
+function toggleDropdown (event) {
+  const dropdown = event.target.parentElement
+
+  try {
+    if (dropdown.style.height === 'fit-content') {
+      fitFirstChild(dropdown)
+    } else {
+      dropdown.style.height = 'fit-content'
+      dropdown.style.width = 'fit-content'
+    }
+  } catch (err) {
+    console.error(err)
+  }
+}
+
+document.querySelectorAll('.dropdown > button').forEach(dropdownButton => {
+  dropdownButton.addEventListener('click', toggleDropdown)
+  fitFirstChild(dropdownButton.parentElement); // collapse it by default
+})
